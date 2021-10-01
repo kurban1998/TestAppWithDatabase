77127e5b7625a67c31b9b524625445f32ad7c6e0
Kurban Kurban <kurban.kurban1998@mail.ru>
2021-10-01 21:26:28 +0300

    added changes

---
+++ SkrugaDBUnitTest/SkrugaDBTest/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkrugaDBTest
+{
+   public  class Circle
+    {
+        public int Id { get; set; }
+        public double Radius { get; set; }
+        public double Square { get; set; }
+    }
+}
