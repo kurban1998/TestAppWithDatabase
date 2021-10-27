3a4ebe7ffcc3933f054820f4c324ad0844fb17e6
Kurban Kurbanov <kurban.kurban1998@mail.ru>
2021-10-27 11:22:41 +0300

    Renamed classes

--- TestAppWithData/CircleAreaCalculator/Properties/AssemblyInfo.cs
+++ TestAppWithData/CircleAreaCalculator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("CircleAreaCalculatorNew")]
+[assembly: AssemblyTitle("CircleAreaCalculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CircleAreaCalculatorNew")]
+[assembly: AssemblyProduct("CircleAreaCalculator")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
